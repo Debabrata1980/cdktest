7bbbc18932d8e68466ee1503fcda9da1b91a204c
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-11-23 10:36:15 +0530

    test

--- cdkstack/cdkstack.py
+++ cdkstack/cdkstack.py
@@ -346,17 +346,16 @@ class CdkStack(Stack):
                               instance_identifier = "meta_tag_rds",
                               iops = 3000,
                               max_allocated_storage = 2000,
- #                             performance_insight_encryption_key = ["arn:aws:kms:us-east-1:409599951855:key/20c09f0c-e88a-4b33-aaef-d1e675c3f28e"],
                               port = 3306,
                               publicly_accessible = False,
                               security_groups = [self.security_group_id,"sg-c98b59be"],
- # #                            security_groups = ["sg-c98b59be", "sg-d71df7a0"],
-                              storage_type = rds.StorageType.IO1,
- #                             credentials={"username": "clusteradmin"}
+                              storage_type = rds.StorageType.IO1)
+#                             performance_insight_encryption_key = ["arn:aws:kms:us-east-1:409599951855:key/20c09f0c-e88a-4b33-aaef-d1e675c3f28e"],
+#                             credentials={"username": "clusteradmin"}
  #                             credentials={"username": "clusteradmin"}
  #                             credentials=rds.Credentials.from_secret(secret)
-                              credentials=rds.Credentials.from_secret(self.secret)
-                              )
+ #                             credentials=rds.Credentials.from_secret(self.secret)
+ #                             )
 
 #               my_user_secret = rds.DatabaseSecret(self, "MyUserSecret",
 #                     username="myuser",
